9e6185ca8669863a71227b1f280c31cdd48c7d90
Marcus Lau <marcuslau@fb.com>
2023-09-07 14:04:12 -0700

    First implementation of extract json api call to csv

--- dags/scripts/extract.py
+++ dags/scripts/extract.py
@@ -2,44 +2,44 @@
 import requests
 import json
 import csv
-
-# print(data)
-
-# Now need to write this logic into an Airflow typa methodology to check daily if any updates
-# if so, then airflow starts the download across all json and comphrensive dps
-    # then transforms the data
-        # then loads into a new database with a new version number etc. 
+import pandas as pd
         
+# Steps for extract file:
+# need to pull all data and save it as JSON->CSV in data 
+# extract file will TRIGGER on the difference of hashes       
         
-def export(url):
+def export():
     # function to do a get request to an api endpoint, path the path to write to
     # TODO: take all the downloaded data and insert into a database to apply SQL on
     URL = "https://pogoapi.net/api/v1/api_hashes.json"
     r = requests.get(url=URL)
-     # json_data = r.json()
+    json_data = r.json()
+    # print(type(json_data)) # type dict
+    # df = pd.read_json(json_data)
+    df = pd.DataFrame(json_data)
+    print(df.T)
+    df.T.to_csv('./data/hashes.csv')
     
-    json_data = json.load(r)
+    # json_data = json.load(r)
     
     # TODO: figure out how to write downloaded data into a folder to then upload into MYSQL
     
     # with open('G:\Akhil\jsonoutput.json') as json_file:
     #     jsondata = json.load(json_file)
  
-    data_file = open('data/hashes.csv', 'w', newline='')
-    csv_writer = csv.writer(data_file)
+    # data_file = open('data/hashes.csv', 'w', newline='')
+    # csv_writer = csv.writer(data_file)
     
-    count = 0
-    for data in json_data:
-        if count == 0:
-            header = data.keys()
-            csv_writer.writerow(header)
-            count += 1
-        csv_writer.writerow(data.values())
+    # count = 0
+    # for data in json_data:
+    #     if count == 0:
+    #         header = data.keys()
+    #         csv_writer.writerow(header)
+    #         count += 1
+    #     csv_writer.writerow(data.values())
     
-    data_file.close()
+    # data_file.close()
     # return data
     
-
-
 if __name__ == "__main__":
-    export("https://pogoapi.net/api/v1/api_hashes.json")
+    export()
